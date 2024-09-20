ad76a2020707058f807e4a8d0d45c5e556094822
nzkks <kkshanthosh@gmail.com>
2024-09-20 22:33:22 +1200

    Add basic styles

--- src/components/searchInput/SearchInput.tsx
+++ src/components/searchInput/SearchInput.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 
+import './searchInput.css';
+
 const SearchInput = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div>
-      {/* Pills */}
+    <div className="searchInputWrapper">
+      <div className="inner">
+        {/* Pills */}
 
-      {/* Search Input with suggestions */}
-      <div>
-        <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} placeholder="Search" />
+        {/* Search Input with suggestions */}
+        <div className="inputContainer">
+          <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} placeholder="Search" />
+          {/* Search suggestions */}
+        </div>
       </div>
     </div>
   );
